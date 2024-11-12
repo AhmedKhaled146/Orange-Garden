77056b584d227cc994d75ad0553982e298103d31
AhmedKhaled146 <a7med74yaso@gmail.com>
2024-11-12 03:22:19 +0200

    add sounds

--- src/scenes/SplashScene.js
+++ src/scenes/SplashScene.js
@@ -7,12 +7,31 @@ export default class SplashScene extends BaseScene {
 
     preload() {
         super.preload(); // Import the default preload. (To Get the Background Effects)
+
+        // Load images
         this.load.image('SplashHouse', '../assets/House.png');
         this.load.image('SplashLogo', '../assets/Start Name.png');
+
+        // Load Audios
+        this.load.audio('BackGroundMusic', '../assets/In Game Sound.mp3');
     }
 
     create() {
         super.create(); // Import the default Background.
+
+        // Background Audio
+        const music = this.sound.add('BackGroundMusic', { loop: true, volume: 0.5 });
+        music.play();
+
+        // Splash Screen
+        // We're using '+=10' to move the house up and down the screen.
+        // The 'yoyo: true' makes the house move back and forth.
+        // The 'repeat: -1' makes the house move indefinitely.
+        // The 'ease: 'Sine.easeInOut'' makes the house move smoothly.
+        // The 'targets: SplashHouse' specifies the object to be moved.
+        // The 'y: '+=10'' specifies the amount to move the object up and down.
+        // The 'duration: 1000' specifies the time it takes for the object to move up and down.
+        // The 'repeat: -1' makes the movement repeat indefinitely.
 
         // Cordinates (X ,Y) and The Splash Screen is the image key.
         const SplashHouse = this.add.image(450, 350, 'SplashHouse').setOrigin(0.5);
